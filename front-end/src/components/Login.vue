9b374a3a3b841499ecaff2b13d7db7920da0146e
a289900566 <289900566@qq.com>
2018-08-22 14:30:18 +0800

    完成基本布局和路由嵌套

---
+++ front-end/src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'loginPage',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ front-end/src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>
+      <router-view/>
+    </el-main>
+  </el-container>
+</template>
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+    }
+  }
+
+}
+</script>
+<style scoped>
+
+</style>
